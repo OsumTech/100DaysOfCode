e9b5eb6a8e304a3dd04f6e41aff982521974dd8f
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2022-09-15 23:20:03 +0500

    Create print.py

---
+++ print.py
@@ -0,0 +1,23 @@
+print ("This is my day 1 with python, I'm so excited")
+
+#how to print a line "n" times
+
+print ('Hello World!\nHello world!')
+
+#concatination
+
+print('Hello' + ' Ahmed')
+
+# if we want print the + sign as well
+
+print('Hello "+" Ahmed')
+
+#Input function in Python
+
+input("What is your Name? ")
+
+print('Hello' + ' ' + input ("what is your neme? "))
+
+#how to find length of string
+
+print(len(input("Waht is your name? ")))
