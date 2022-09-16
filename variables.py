f3da87626048e4e9178b21a8db8d30279bfa7b87
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2022-09-16 16:42:30 +0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,19 @@
+"""
+name = input("whats is your name? ")
+print(name)
+
+name1 = "abc"
+print(name1)
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
+"""
+
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
