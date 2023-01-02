2fab4da0c35755f9fec00ad52c1b4d339e340ea8
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-02 23:01:37 +0500

    Update DAY7-Hangman.py

--- DAY7-Hangman.py
+++ DAY7-Hangman.py
@@ -4,19 +4,21 @@ word_list = ["aardvark", "baboon", "camel", "happy", ]
 
 #TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
 
+import random
+
+chosen_word = random.choice(word_list)
+print (chosen_word)
+
 #TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+guess_word = input("Please Enter Your Guess: ").lower() 
 
 #TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
 
-import random
-
-
-chosen_word = random.choice(word_list)
-
-print (chosen_word)
-
-guess_word = input("Please Enter Your Guess: ").lower()
-
-result_word = []
+for letter in chosen_word:
+    if letter == guess_word:
+        print ("Right")
+    else:
+        print("Wrong")
 
 
