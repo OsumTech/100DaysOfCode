2e6cc89602ff146622d44b7236a5f67fa1acda49
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-03 22:47:26 +0500

    Update DAY7-Hangman.py

--- DAY7-Hangman.py
+++ DAY7-Hangman.py
@@ -16,26 +16,29 @@ print (chosen_word)
 #For each letter in the chosen_word, add a "_" to 'display'.
 #So if the chosen_word was "apple", display should be ["_", "_", "_", "_", "_"] with 5 "_" representing each letter to guess.
 
-guess = input("Guess a letter: ").lower()
+
 
 display_word =[]
 
 for letter in chosen_word:
     display_word.append("_")
-    
 
-#TODO-2: - Loop through each position in the chosen_word;
-#If the letter at that position matches 'guess' then reveal that letter in the display at that position.
-#e.g. If the user guessed "p" and the chosen word was "apple", then display should be ["_", "p", "p", "_", "_"].
-i = 0
-for letter in chosen_word:
-    i += 1
-    if letter == guess:
+
+
+#step3
+
+#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+
+
+dash = "_"
+while dash in display_word:
+    guess = input("Guess a letter: ").lower()
+    i=0
+    for letter in chosen_word:
+     if letter == guess:
         display_word[i] = guess
+     i += 1
+    print(display_word)   
+else:
+    print("Congratulations!!! You Have Won!")
 
-
-
-#TODO-3: - Print 'display' and you should see the guessed letter in the correct position and every other letter replace with "_".
-#Hint - Don't worry about getting the user to guess the next letter. We'll tackle that in step 3.
-
-print(display_word)
