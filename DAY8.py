9c787c6eac43b814ea4d4f1791e5ad68b6bfc079
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-05 23:43:27 +0500

    Update DAY8.py

--- DAY8.py
+++ DAY8.py
@@ -1,6 +1,12 @@
-def greet(name):
-    print(f"Hi There! {name}")
-    print(f"Its Nice to see you back {name}")
-    print("Lets have some fun.")
+# def greet(name):
+#     print(f"Hi There! {name}")
+#     print(f"Its Nice to see you back {name}")
+#     print("Lets have some fun.")
 
-greet("ahmed")
+# greet("ahmed")
+
+def greet(name, location):
+    print(f"Hi There {name}")
+    print(f"Weather is soo good here in {location}")
+
+greet("ahmed","faislabad")
