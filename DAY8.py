f247781f2cd56b806bd11ec73318946ad75091aa
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-05 23:29:24 +0500

    Update DAY8.py

--- DAY8.py
+++ DAY8.py
@@ -1,6 +1,6 @@
-def greet():
-    print("Hi There!")
-    print("Its Nice to see you back")
+def greet(name):
+    print(f"Hi There! {name}")
+    print(f"Its Nice to see you back {name}")
     print("Lets have some fun.")
 
-greet()
+greet("ahmed")
