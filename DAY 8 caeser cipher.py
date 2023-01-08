11c5442846461aa1b1d7ad2214c0768a23f27ba0
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-08 12:35:31 +0500

    Update DAY 8 caeser cipher.py

--- DAY 8 caeser cipher.py
+++ DAY 8 caeser cipher.py
@@ -6,51 +6,53 @@ shift = int(input("Type the shift number:\n"))
 
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-def encrypt(message,number):
+def caesar(message,number,todo):
+    def encrypt(message,number):
 
     #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    text_list = list(message)
-    cipher = []
-    items = len(text_list)
-    for i in range (0,items):
-        letter = text_list[i]
-        if letter in alphabet:
-            alpha = alphabet.index(letter)
-            alpha = alpha + number
-            if alpha >25:
-                alpha -= 26
-                word = alphabet[alpha]
-                cipher.append(word)
-            else:
-                word = alphabet[alpha]
-                cipher.append(word)
-    print(" ".join(cipher))
+        text_list = list(message)
+        cipher = []
+        items = len(text_list)
+        for i in range (0,items):
+            letter = text_list[i]
+            if letter in alphabet:
+                alpha = alphabet.index(letter)
+                alpha = alpha + number
+                if alpha >25:
+                    alpha -= 26
+                    word = alphabet[alpha]
+                    cipher.append(word)
+                else:
+                    word = alphabet[alpha]
+                    cipher.append(word)
+        print(" ".join(cipher))
 
     #e.g. 
     #plain_text = "hello"
     #shift = 5
     #cipher_text = "mjqqt"
     #print output: "The encoded text is mjqqt"
-def decrypt(message,number):
-    text_list = list(message)
-    cipher = []
-    items = len(text_list)
-    for i in range (0,items):
-        letter = text_list[i]
-        if letter in alphabet:
-            alpha = alphabet.index(letter)
-            alpha = alpha - number
-            if alpha == 0:
-                alpha += 26
-                word = alphabet[alpha]
-                cipher.append(word)
-            else:
-                word = alphabet[alpha]
-                cipher.append(word)
-    print(" ".join(cipher))
+    def decrypt(message,number):
+            text_list = list(message)
+            cipher = []
+            items = len(text_list)
+            for i in range (0,items):
+                letter = text_list[i]
+                if letter in alphabet:
+                    alpha = alphabet.index(letter)
+                    alpha = alpha - number
+                    if alpha == 0:
+                        alpha += 26
+                        word = alphabet[alpha]
+                        cipher.append(word)
+                    else:
+                        word = alphabet[alpha]
+                        cipher.append(word)
+            print(" ".join(cipher))
+    
+    if direction == "encode":
+        encrypt(message=text,number=shift)
+    elif direction == "decode":
+        decrypt(message=text,number=shift)
 
-if direction == "encode":
-    encrypt(message=text,number=shift)
-elif direction == "decode":
-    decrypt(message=text,number=shift)
+caesar(message=text,number=shift,todo=direction)
