926198dc2e2672aed9588931fcdfd2896abda877
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-16 19:18:06 +0500

    Update DAY9-Grading.py

--- DAY-9-Grading./DAY9-Grading.py
+++ DAY-9-Grading./DAY9-Grading.py
@@ -8,11 +8,22 @@ student_scores = {
 # 🚨 Don't change the code above 👆
 
 #TODO-1: Create an empty dictionary called student_grades.
-
+student_grades={}
 
 #TODO-2: Write your code below to add the grades to student_grades.👇
 
+for key in student_scores:
+    marks = student_scores[key]
+    student_grades[key] = marks
+    if marks >= 90:
+        student_grades[key] = "Ouststanding"
+    elif marks > 80:
+        student_grades[key] = "Exceeds Expectations"
+    elif marks > 70:
+        student_grades[key] = "Acceptable"
+    elif marks <= 70:
+        student_grades[key] = "Fail"
     
-
+    
 # 🚨 Don't change the code below 👇
 print(student_grades)
