12425e2d020f261920db351ebcbb412a4a09c6b1
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-17 19:54:02 +0500

    Update DAY-9-Nested list & Dic.py

--- DAY-9-Nested list & Dic.py
+++ DAY-9-Nested list & Dic.py
@@ -1,6 +1,16 @@
-travel_log = {
-    "France": {"cities_visited": ["Paris", "Lille", "Dijon"], "tatoal_visited": 12},
-    "Germany": {"Cities_Visited":["Berlin", "Hamburg", "Stuttgart"],"Total_visits":5}
-}
+travel_log = [
+    {
+        {
+            "Country":"France",
+            "cities_visited": ["Paris", "Lille", "Dijon"], "tatoal_visited": 12
+        },
+        {
+            "Country":"Germany", 
+            "Cities_Visited": ["Berlin", "Hamburg", "Stuttgart"],"Total_visits":5
+        }
+    }
+]
+
+
 
 print(travel_log["France"])
