017faa84060d1f5ce600b1a46c694856140dda3d
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-21 22:34:22 +0500

    Create DAY4-lists.py

---
+++ DAY4-lists.py
@@ -0,0 +1,21 @@
+import random
+
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", 
+"South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", 
+"Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", 
+"Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", 
+"South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america[-1])
+
+
+states_of_america[1] = "pencilvinia"
+
+#to add new item in the list
+states_of_america.append("Ahmed's land")
+
+#to extend the list
+states_of_america.extend(["Faisalabad", "tudiwala", "bole ki chugi", "mai ki chugi"])
+
+
+print(states_of_america)
