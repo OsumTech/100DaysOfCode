8e7ec4e12655c31b31420a7a7d85fabd3e18b21b
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-21 22:35:17 +0500

    Create DAY4-random number.py

---
+++ DAY4-random number.py
@@ -0,0 +1,12 @@
+import random
+
+random_integer =random.randint(1, 10)
+print(random_integer)
+
+#For Floats
+
+random_float = random.random()
+print(random_float)
+
+love = random.randint(1, 100)
+print(f"Your Love Score Is {love}")
