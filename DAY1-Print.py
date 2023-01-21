e503ce605da0c20e5e82c87b591dff659ce5c127
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-21 22:24:13 +0500

    Create DAY1-Print.py

---
+++ DAY1-Print.py
@@ -0,0 +1,8 @@
+print("Day 1 - Python Print Functin")
+print("The Function is declared this way:")
+print("print('What to Print')")
+
+# String Concatenation
+
+print("Hello World!\nHello World!\nHello World!")
+print("Hello" + " Ahmed")
