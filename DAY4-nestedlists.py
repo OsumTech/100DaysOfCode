4d8a377ac7d2e15454f59257b7a46878f86d484f
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-21 22:34:46 +0500

    Create DAY4-nestedlists.py

---
+++ DAY4-nestedlists.py
@@ -0,0 +1,6 @@
+fruits = ["strawberries", "netcarines", "Apples", "grapes", "peaches", "cherries", "pears"]
+vegetables = ["spinach", "kale", "tomatoes", "celery", "potatoes"]
+
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
