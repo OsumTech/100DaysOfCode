1705d4e0e2532ba1dcfe4fe6cc48ff0f3b51c310
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-21 22:25:53 +0500

    Create DAY1-debugging.py

---
+++ DAY1-debugging.py
@@ -0,0 +1,4 @@
+print("Day 1 - String Manipulation")
+print("String Concatenation is done With The" + "+ Sign")
+print("e.g print('Hello' + 'World')")
+print("New Lines can be created with a back lash & a n.")
