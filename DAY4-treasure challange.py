688312b0a44ad865fc5c0b5efe49427b152c82be
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-21 22:36:32 +0500

    Create DAY4-treasure challange.py

---
+++ DAY4-treasure challange.py
@@ -0,0 +1,9 @@
+row1 = ["🎯", "🎯", "🎯"]
+row2 = ["🎯", "🎯", "🎯"]
+row3 = ["🎯", "🎯", "🎯"]
+
+map = [row1, row2, row3]
+
+
+print(f" {row1} \n {row2} \n {row3}")
+position = input("Where do yo want pur the treasure?  ")
