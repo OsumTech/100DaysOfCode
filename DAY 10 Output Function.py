469f3c5ec7698f3e9fd0fe8a31b840fd5d1763b5
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2023-01-25 22:29:23 +0500

    Update DAY 10 Output Function.py

--- DAY 10 Output Function.py
+++ DAY 10 Output Function.py
@@ -1,1 +1,9 @@
+f_name = input("Please Enter Your First Name: ")
+l_name = input("Please Enter Your Last Name: ")
 
+def format_name(f_name, l_name):
+    f_name = f_name.title()
+    l_name = l_name.title()
+    print(f"Your Name is {f_name} {l_name}")
+
+format_name(f_name, l_name)
